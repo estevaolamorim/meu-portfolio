fcff4b069009a8803e353beb9dde2bb0a17fe28e
Estevão <estevaol.amorim@gmail.com>
2026-09-09 21:35:47 -0300

    Add welcome message display on DOMContentLoaded

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    exibirMensagemBoasVindas();
+});
+
+function exibirMensagemBoasVindas(){
+    const mensagem = "Olá, recrutador! O portfólio do Estevão foi carregado com sucesso.";
+    console.log(mensagem);
+}
